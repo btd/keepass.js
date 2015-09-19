46db627d4a8ded5a2e7e98145a55f7f377768162
Denis Bardadym <bardadymchik@gmail.com>
2015-09-19 22:56:12 +0300

    Initial working release
    It is possible to create db from zero, write it to buffer and read from buffer.

---
+++ README.md
@@ -0,0 +1,8 @@
+keepass.js
+==========
+
+[![License](http://img.shields.io/badge/license-GPLv3-blue.svg)](https://github.com/btd/keepass.js/blob/master/LICENSE.md)
+
+Copyright
+---------
+Copyright &copy; 2015 Denis Bardadym. All rights reserved.
---
+++ examples/001_basic_usage.js
@@ -0,0 +1,68 @@
+var path = require('path');
+
+var Password = require('../src/key/Password');
+var File = require('../src/key/File');
+
+var Promise = require('es6-promise').Promise;
+function promisify(f) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    return new Promise(function(resolve, reject) {
+      args.push(function(err) {
+        if(err) return reject(err);
+
+        var res = Array.prototype.slice.call(arguments, 1);
+        if(res.length == 0) resolve();
+        else if(res.length == 1) resolve(res[0]);
+        else resolve(res);
+      });
+      f.apply(null, args);
+    });
+  }
+}
+var readFile = promisify(require('fs').readFile)
+var writeFile = promisify(require('fs').writeFile);
+
+var io = require('../src/keepass/IO');
+var Reader = io.Reader;
+var Writer = io.Writer;
+
+// Detect some needed paths
+var resourcePath = path.join(__dirname, '..', 'test', 'resources');
+//var databasePath = path.join(resourcePath, '000_example.kdbx');
+var databasePath = path.join(__dirname, '..', 'test.kdbx');
+var newDatabasePath = path.join(__dirname, 'new-example.kdbx');
+var keyfilePath = path.join(resourcePath, '000_example.key');
+
+
+Promise.all([readFile(databasePath), readFile(keyfilePath)])
+  .then(function(args) {
+    var buf = args[0];
+    var key = args[1];
+    var r = new Reader([
+      new Password('123456')
+      //new Password('nebuchadnezzar'),
+      //new File(key)
+    ]);
+    return r.buffer(buf);
+  })
+  .then(function(db) {
+    db.name = 'Keepass js works!';
+    return db;
+  })
+  .then(function(db) {
+    return Promise.all([ db, readFile(keyfilePath) ])
+  })
+  .then(function(args) {
+    var keyfile = args[1];
+    var db = args[0];
+    var w = new Writer([
+      new File(keyfile),
+      new Password('morpheus')
+    ]);
+
+    return w.buffer(db);
+  })
+  .then(function(buf) {
+    return writeFile(newDatabasePath, buf);
+  })
